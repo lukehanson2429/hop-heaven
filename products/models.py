1f4c66639001835c8ed9294a489f92039d445a4f
lukehanson2429 <lukehanson2429@gmail.com>
2021-09-14 16:36:56 +0000

    product models updated (brewery field max length increased), rating set on new line in product details

--- products/models.py
+++ products/models.py
@@ -21,7 +21,7 @@ class Product(models.Model):
     name = models.CharField(max_length=254)
     description = models.TextField()
     country = models.CharField(max_length=15)
-    brewery = models.CharField(max_length=15)
+    brewery = models.CharField(max_length=20)
     abv = models.DecimalField(max_digits=6, decimal_places=2)
     size = models.IntegerField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
