493f6866f36392ba9381706ec3b07ab78522bb08
lukehanson2429 <lukehanson2429@gmail.com>
2021-09-23 12:53:15 +0000

    Added stripe elements & css styling to form fields, buttons

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51JNaLHH94UZnjaaVSd3wlFO5waMiKt8J8nNxEQTFJ2x83mZAg4hKFOVSYqJpsPpDQVTsQAWq1rBEqVZXi4ceOiwz00M4QOZkOC',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
