c283016d2bf90403e733c62068d8f242d5d0a8de
lukehanson2429 <lukehanson2429@gmail.com>
2021-10-05 13:43:15 +0000

    review model updated, ReviewForm created with custom css styling & added to template/views

--- products/forms.py
+++ products/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from .widgets import CustomClearableFileInput
-from .models import Product, Category
+from .models import Product, Category, Review
 
 
 class ProductForm(forms.ModelForm):
@@ -19,3 +19,25 @@ class ProductForm(forms.ModelForm):
         self.fields['category'].choices = friendly_names
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'border-black rounded-0 product-form-input'
+
+
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = Review
+        fields = ('content', 'rating')
+
+    def __init__(self, *args, **kwargs):
+        """
+        Add placeholders and classes.
+        """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'content': 'Content',
+            'rating': 'Rating',
+        }
+
+        for field in self.fields:
+            placeholder = placeholders[field]
+            self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = 'review-form-input border-black rounded-0'
+            self.fields[field].label = False
--- products/models.py
+++ products/models.py
@@ -43,7 +43,8 @@ class Product(models.Model):
         else:
             return 0
 
-class ProductReview(models.Model):
+
+class Review(models.Model):
     product = models.ForeignKey(Product, related_name='reviews', on_delete=models.CASCADE)
     user = models.ForeignKey(User, related_name='reviews', on_delete=models.CASCADE)
     content = models.TextField(blank=True, null=True)
