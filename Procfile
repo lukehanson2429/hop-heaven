06dfaacb3fefe08e73f922626c74f9373803d0d7
lukehanson2429 <lukehanson2429@gmail.com>
2021-10-12 15:11:00 +0000

    Procfiles added and allowed hosts/database settings updated

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn hop_heaven.wsgi:application
--- hop_heaven/settings.py
+++ hop_heaven/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-cxsl6a_gxv%p15g%s&s+r2hwxn6$hc*%4@_bgu8zhhu3-4s38#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['hop-heaven.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -121,10 +121,15 @@ WSGI_APPLICATION = 'hop_heaven.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
 
